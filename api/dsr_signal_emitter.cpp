fb64f379dd73a55d53f9f584bb2a1435dc2ec3ee
juancarlosgg <juancarlos97gg@gmail.com>
2026-02-16 13:08:13 +0100

    debug: add sync messages

--- api/dsr_signal_emitter.cpp
+++ api/dsr_signal_emitter.cpp
@@ -1,11 +1,12 @@
 #include <dsr/api/dsr_api.h>
 #include <dsr/api/dsr_signal_emitter.h>
 #include <dsr/api/dsr_signal_info.h>
+#include <qdebug.h>
 
 void DSR::QueuedSignalRunner::run_update_node_signal(std::uint64_t a,
                                                      const std::string &b,
                                                      SignalInfo c) {
-
+  qDebug() << "[SIGNAL] update_node id:" << a << "type:" << QString::fromStdString(b);
   tp.spawn_task([=, this] {
     for (auto fn : uns_fns) {
       if (fn)
@@ -15,6 +16,7 @@ void DSR::QueuedSignalRunner::run_update_node_signal(std::uint64_t a,
 }
 void DSR::QueuedSignalRunner::run_update_node_attr_signal(
     std::uint64_t a, const std::vector<std::string> &b, SignalInfo c) {
+  qDebug() << "[SIGNAL] update_node_attr id:" << a;
   tp.spawn_task([=, this] {
     for (auto fn : unas_fns) {
       if (fn)
@@ -26,6 +28,7 @@ void DSR::QueuedSignalRunner::run_update_edge_signal(std::uint64_t a,
                                                      std::uint64_t b,
                                                      const std::string &c,
                                                      SignalInfo d) {
+  qDebug() << "[SIGNAL] update_edge from:" << a << "to:" << b << "type:" << QString::fromStdString(c);
   tp.spawn_task([=, this] {
     for (auto fn : ues_fns) {
       if (fn)
@@ -37,6 +40,7 @@ void DSR::QueuedSignalRunner::run_update_edge_signal(std::uint64_t a,
 void DSR::QueuedSignalRunner::run_update_edge_attr_signal(
     std::uint64_t a, std::uint64_t b, const std::string &c,
     const std::vector<std::string> &d, SignalInfo e) {
+  qDebug() << "[SIGNAL] update_edge_attr from:" << a << "to:" << b << "type:" << QString::fromStdString(c);
   tp.spawn_task([=, this] {
     for (auto fn : ueas_fns) {
       if (fn)
@@ -49,6 +53,7 @@ void DSR::QueuedSignalRunner::run_del_edge_signal(std::uint64_t a,
                                                   std::uint64_t b,
                                                   const std::string &c,
                                                   SignalInfo d) {
+  qDebug() << "[SIGNAL] del_edge from:" << a << "to:" << b << "type:" << QString::fromStdString(c);
   tp.spawn_task([=, this] {
     for (auto fn : des_fns) {
       if (fn)
@@ -58,6 +63,7 @@ void DSR::QueuedSignalRunner::run_del_edge_signal(std::uint64_t a,
 }
 void DSR::QueuedSignalRunner::run_del_node_signal(std::uint64_t a,
                                                   SignalInfo b) {
+  qDebug() << "[SIGNAL] del_node id:" << a;
   tp.spawn_task([=, this] {
     for (auto fn : den_fns) {
       if (fn)
@@ -68,6 +74,7 @@ void DSR::QueuedSignalRunner::run_del_node_signal(std::uint64_t a,
 
 void DSR::QueuedSignalRunner::run_deleted_node_signal(const Node &a,
                                                       SignalInfo b) {
+  qDebug() << "[SIGNAL] deleted_node name:" << QString::fromStdString(a.name()) << "id:" << a.id();
   tp.spawn_task([=, this] {
     for (auto fn : dn_fns) {
       if (fn)
@@ -77,6 +84,7 @@ void DSR::QueuedSignalRunner::run_deleted_node_signal(const Node &a,
 }
 void DSR::QueuedSignalRunner::run_deleted_edge_signal(const Edge &a,
                                                       SignalInfo b) {
+  qDebug() << "[SIGNAL] deleted_edge from:" << a.from() << "to:" << a.to() << "type:" << QString::fromStdString(a.type());
   tp.spawn_task([=, this] {
     for (auto fn : de_fns) {
       if (fn)
